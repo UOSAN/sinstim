7ce9b62da16279a9c90e19b597a4401d8ecc44cf
Erik May <may.erik@gmail.com>
2018-06-19 21:52:11 -0700

    Created node script to generate a sql script to insert picture data

---
+++ ClientApp/populatePictureTable.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const _ = require('lodash');
+const uuidv4 = require('uuid/v4');
+
+const picturesAbsolutePath = 'C:\\Users\\Erik\\workspace\\SinStim\\SamplePictures';
+
+let insertScript = '';
+
+const categories = fs.readdirSync(picturesAbsolutePath);
+_.forEach(categories, (category) => {
+    const files = fs.readdirSync(path.join(picturesAbsolutePath, category));
+    _.forEach(files, (fileName) => {
+        insertScript +=
+        `
+        INSERT INTO [dbo].[PICTURES]
+            ([ID], [PATH], [FILE_NAME], [CATEGORY])
+        VALUES
+            ('${uuidv4()}', 'path', '${fileName}', '${category}')
+        GO
+        `
+    });
+});
+
+fs.writeFile('insertPictureData.sql', insertScript, (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+

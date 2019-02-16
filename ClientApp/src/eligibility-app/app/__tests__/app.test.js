e22d7365601e879cbf4ca54173f8186fa660684a
Erik May <erik.may@emberex.com>
2019-02-15 18:50:06 -0800

    fix unit tests

--- ClientApp/src/eligibility-app/app/__tests__/app.test.js
+++ ClientApp/src/eligibility-app/app/__tests__/app.test.js
@@ -3,8 +3,16 @@ import { shallow } from 'enzyme';
 
 import App from '../app';
 
+let props;
+
+beforeEach(() => {
+    props = {
+        onSaveUser: jest.fn(() => Promise.resolve())
+    };
+});
+
 it('should match snapshot', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<App {...props} />);
 
     expect(wrapper).toMatchSnapshot();
 });

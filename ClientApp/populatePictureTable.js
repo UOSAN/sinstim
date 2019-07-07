50d7d880f7cc8e7660a27cf1ff5c3f9a76ce718a
Erik May <erik.may@emberex.com>
2019-07-06 18:17:01 -0700

    adding more real picture data

--- ClientApp/populatePictureTable.js
+++ ClientApp/populatePictureTable.js
@@ -3,24 +3,68 @@ const path = require('path');
 const endOfLine = require('os').EOL;
 const _ = require('lodash');
 const uuidv4 = require('uuid/v4');
+const glob = require('glob');
+
+const categoryDictionary = {
+    alcohol: 'alcohol',
+    bowls: 'neutral',
+    buttons: 'neutral',
+    cameras: 'neutral',
+    chairs: 'neutral',
+    chocolate: 'chocolate',
+    clocks: 'neutral',
+    cocaine: 'cocaine',
+    cookies: 'cookies',
+    donuts: 'donuts',
+    fans: 'neutral',
+    fries: 'fries',
+    heroin: 'heroin',
+    iceCream: 'iceCream',
+    lights: 'neutral',
+    marijuana: 'marijuana',
+    meth: 'meth',
+    pasta: 'pasta',
+    pens: 'neutral',
+    phones: 'neutral',
+    pills: 'pill',
+    pizza: 'pizza',
+    radios: 'neutral',
+    teapots: 'neutral',
+    tobacco: 'tobacco',
+    toothbrushes: 'neutral',
+    umbrellas: 'neutral'
+};
 
 const picturesAbsolutePath = path.join(__dirname, '..', 'SamplePictures');
-const categories = fs.readdirSync(picturesAbsolutePath);
-
+const isDirectory = (filePath) => fs.statSync(filePath).isDirectory();
+const getDirectories = (src, callback) => {
+    glob(`${src}/**/*`, callback);
+};
 let insertScript = 'INSERT INTO PICTURES (ID, PATH, FILENAME, CATEGORY) VALUES ';
 
-_.forEach(categories, (category) => {
-    const files = fs.readdirSync(path.join(picturesAbsolutePath, category));
-
-    _.forEach(files, (fileName) => {
-        insertScript += `${endOfLine}('${uuidv4()}', 'pictures/', '${fileName}', '${category}'),`;
-    });
-});
-insertScript = insertScript.slice(0, -1);
-
-fs.writeFile('insertPictureData.sql', insertScript, (err) => {
+getDirectories(picturesAbsolutePath, (err, filePaths) => {
     if (err) {
-        return console.log(err);
+        console.log('Error', err);
+    } else {
+        _.forEach(filePaths, (filePath) => {
+            if (!isDirectory(filePath)) {
+                const pictureFileName = path.basename(filePath);
+                const pictureFullFilePath = filePath.substring(filePath.indexOf(`SamplePictures${path.sep}`) + `SamplePictures${path.sep}`.length);
+                const pictureFilePath = pictureFullFilePath.substring(0, _.lastIndexOf(pictureFullFilePath, path.sep));
+
+                const category = categoryDictionary[pictureFileName.split('_')[0]];
+
+                insertScript += `${endOfLine}('${uuidv4()}', '${pictureFilePath}', '${pictureFileName}', '${category}'),`;
+            }
+        });
+
+        insertScript = insertScript.slice(0, -1);
+
+        fs.writeFile('insertPictureData.sql', insertScript, (e) => {
+            if (e) {
+                return console.log(e);
+            }
+            console.log('Script Generated!');
+        });
     }
-    console.log('Script Generated!');
 });

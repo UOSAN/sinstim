e281cbdbf0168a02a6f4af12e3afab1c2a0a94e9
Erik May <erik.may@emberex.com>
2019-08-04 21:19:07 -0700

    comment out logging code

--- ClientApp/src/admin-app/index.js
+++ ClientApp/src/admin-app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import 'bulma/css/bulma.min.css/';
@@ -10,12 +10,12 @@ import 'bulma/css/bulma.min.css/';
 import sinStimReducers from '../state/reducers/reducers';
 import App from './app';
 
-const logger = createLogger();
+// const logger = createLogger();
 const store = createStore(
     sinStimReducers,
     compose(
         applyMiddleware(thunk),
-        applyMiddleware(logger)
+        // applyMiddleware(logger)
     )
 );
 
